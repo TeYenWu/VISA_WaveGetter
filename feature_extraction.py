0f04c81645865526565c974a9714647f4c9afc9d
lee1258561 <b02902083@ntu.edu.tw>
2017-03-10 17:10:54 +0800

    feature extraction initinal commit. extract spectrum feature from wave.

---
+++ feature_extraction.py
@@ -0,0 +1,63 @@
+import json
+import matplotlib.pyplot as plt
+import scipy.fftpack
+import numpy as np
+from scipy.fftpack import dct
+
+def wave_ceptrum(waves, N, sample_rate):
+    pow_spec = power_spectrum(waves)
+    
+    fb = filterbank(26, N, sample_rate)
+    feat = np.dot(pow_spec, fb.T)
+    #need energy?
+
+    feat = np.log(feat)
+    feat = dct(feat, type=2, axis=1, norm='ortho')[:,:numcep]
+
+    return feat
+
+def power_spectrum(waves, n=None):
+    if n == None:
+        n = waves.shape[-1] 
+
+    mag_spec = np.absolute(np.fft.rfft(waves, n))
+    return 1.0 / n * np.square(mag_spec)
+
+def filterbank(filt_num, N, sample_rate=16000, low_freq=0, high_freq=None):
+    
+    highfreq = highfreq or samplerate / 2
+    assert highfreq <= samplerate/2, "highfreq is greater than samplerate/2"
+    
+    filt_points = np.linspace(low_freq, high_freq, filt_num + 1)
+    bin = np.floor(N * filt_points / sample_rate)
+
+    #rectangular filter
+    fbank = np.zeros(filt_num, N // 2 + 1)
+    for i in range(filt_num):
+        for j in range(int(bin[i]), int(bin[i + 1]):
+            fbank[i, j] = 1
+
+
+def get_wave_feature(waves, sample_rate):
+    pass
+
+
+
+if __name__ == "__main__":
+    N = 60000
+    sample_rate = 80000
+
+    T = 1.0 / sample_rate
+
+    x = np.linspace(0, N * T, N)
+    y = np.sin(100.0 * 2.0 * np.pi * x) + 0.5 * np.sin(150 * 2.0 * np.pi * x)
+    
+    print('start')
+    yf = power_spectrum(y)
+    print('done')
+    xf = np.linspace(0, 1.0 / (2.0 * T), N / 2 + 1)
+
+    plt.plot(xf[:100], yf[:100])
+    plt.show()
+
+
